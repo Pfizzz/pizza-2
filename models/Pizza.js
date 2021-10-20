47817c7a3c4f3a037463e3714b3f43854b265624
Pfizz <pfiste80@gmail.com>
2021-10-20 19:45:11 -0400

    end of lesson 1

---
+++ models/Pizza.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+// we only need the schema constructor and model function
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model('Pizza', PizzaSchema);
+
+// export the pizza model 
+module.exports = Pizza;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+const Pizza = require('./Pizza');
+
+// this file packages up all the models.
+
+module.exports = { Pizza };
